399e3c1eb393c70de77ebeb502a07efe1b2e2e9f
pelurululu <muhdzulkml144@gmail.com>
2026-05-04 16:56:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -894,6 +894,11 @@ document.querySelectorAll('.modal-overlay').forEach(m => {
   </div>
 </div>
 
+  <script>
+  const supabaseUrl = '<?php echo getenv("SUPABASE_URL"); ?>';
+  const supabaseKey = '<?php echo getenv("SUPABASE_ANON_KEY"); ?>';
+</script>
+
 <?php include 'footer.php'; ?>
 
 </body>
