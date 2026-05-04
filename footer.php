31524985b87d9bc66085f81c550bbb4cf3ee8a16
pelurululu <muhdzulkml144@gmail.com>
2026-05-04 15:55:10 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -8,14 +8,11 @@
     <div class="footer-bottom">
       <div class="footer-copyright">
         &copy; 2025 <strong>Jodohku.my</strong>. Hak Cipta Terpelihara.<br>
-        <span style="font-size:12px;color:#444;">Didaftarkan di bawah undang-undang Malaysia &nbsp;&bull;&nbsp; SSM No. [Pending Registration] &nbsp;&bull;&nbsp; Platform ini mematuhi PDPA 2010</span>
+        
       </div>
 
       <div style="text-align:center;">
         <div class="footer-legal-links">
-          <a href="#" onclick="openModal('termsModal');return false;">Terma &amp; Syarat</a>
-          <a href="#" onclick="openModal('privacyModal');return false;">Dasar Privasi</a>
-          <a href="#">Polisi Kuki</a>
         </div>
       </div>
 
@@ -35,7 +32,7 @@
         Jodohku.my adalah jenama di bawah portfolio <strong style="color:#555;">Noble Synergy Ventures</strong>
       </div>
       <div style="font-size:11px;color:#2a2a2a;letter-spacing:1px;text-transform:uppercase;">
-        Noble Synergy Ventures &nbsp;&#9670;&nbsp; Building Tomorrow's Brands Today
+        Noble Synergy Ventures &nbsp;&#9670;&nbsp; Islamic Marriage Platform
       </div>
     </div>
   </div>
