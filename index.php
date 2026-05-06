3f84206d702e32b84daeb37ff873e21678a65f98
pelurululu <muhdzulkml144@gmail.com>
2026-05-06 17:23:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;0,700;1,400&family=Tajawal:wght@300;400;500;700&family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="styles.css">
-<link rel="icon" type="images/logokut.png" href="favicon.png">
+<link rel="icon" type="image/png" href="logokut.png">
 <link rel="apple-touch-icon" href="logokut.png">
 <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
 
